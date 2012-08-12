b94b12fc63d7f9c430eea204cf50a3abe8c31ba5
Wilson Cheng <wilson.system1@gmail.com>
2012-08-12 18:39:38 +0800

    CH07-Finish User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp1::Application.routes.draw do
 
-  get "users/new"
+  resources :users 
+
+  #get "users/new"
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
