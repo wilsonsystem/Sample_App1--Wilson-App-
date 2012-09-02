3ab6e94704f5e802168644cd7a3c542d774c62ea
Wilson Cheng <wilson.system1@gmail.com>
2012-09-03 01:01:30 +0800

    CH-08 FInish Signin without 8.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   	if @user.save
 
   	   flash[:success] = "Welcome to the Sample App!"
+       sign_in @user
   	   redirect_to @user
   	else 
   	   render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,20 @@ SampleApp1::Application.routes.draw do
 
   resources :users 
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   #get "users/new"
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup',  to: 'users#new' 
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   root to: 'static_pages#home'
+
+
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
