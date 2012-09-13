33d93137c4e0e327a23b63e42387ea33fca1a086
Wilson Cheng <wilson.system1@gmail.com>
2012-09-14 01:40:07 +0800

    CH-09 Pages

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     name     "Michael Hartl"
     email    "michael@example.com"
-    password "foobar"
-    password_confirmation "foobar"
+    password "123456"
+    password_confirmation "123456"
   end
 end
